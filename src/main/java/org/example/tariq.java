a0e96345619793913810c1ed68ff6d216e6941b3
Mihir Sahu <2002mihir@gmail.com>
2022-05-18 16:49:27 -0500

    Test 5

--- src/main/java/org/example/tariq.java
+++ src/main/java/org/example/tariq.java
@@ -3,5 +3,7 @@ package org.example;
 public class tariq {
     public static void main(String[] args) {
         System.out.println("Hello Mr Tariq I have been expecting you");
+		int a = 0;
+		System.out.println(a);
     }
 }
